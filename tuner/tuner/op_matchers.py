9c7b9ecfc3ab8aae1f923aac986eaa71c2f0c684
Max Dawkins <max.dawkins@gmail.com>
2025-01-30 11:50:24 -0600

    [Tuner] Add ConvolutionDimensions inference to conv parser
    Signed-off-by: Max Dawkins <max.dawkins@gmail.com>

--- tuner/tuner/op_matchers.py
+++ tuner/tuner/op_matchers.py
@@ -93,7 +93,7 @@ class GenericOpMatcher(NamedOpMatcher):
 
         maps_attr = None
         for attr in op.opview.attributes:
-            if attr.name == "indexing_maps" and isinstance(attr.attr, ir.ArrayAttr):
+            if (attr.name == "indexing_maps" or attr.name == "linalg.memoized_indexing_maps") and isinstance(attr.attr, ir.ArrayAttr):
                 maps_attr = attr.attr
         if maps_attr is None:
             return False
@@ -122,6 +122,108 @@ def get_map_result_dim_positions(map: ir.AffineMap):
         dim_position = int(dim_str[1:])
         exprs.append(dim_position)
     return exprs
+
+
+def get_convolution_dims(lhs_map: ir.AffineMap, rhs_map: ir.AffineMap, res_map: ir.AffineMap):
+    if not rhs_map.is_projected_permutation or not res_map.is_projected_permutation:
+        return None
+    lhs_dims = []
+    rhs_dims = get_map_result_dim_positions(rhs_map)
+    res_dims = get_map_result_dim_positions(res_map)
+    batch_dims = []
+    input_channel_dims = []
+    output_channel_dims = []
+    output_img_dims = []
+    filter_loop_dims = []
+    strides = []
+    dilations = []
+    
+    def get_dim_pos(dim):
+        if len(dim) < 1 or dim[0] != "d" or not dim[1:].isdigit():
+            return None
+        return int(dim[1:])
+
+    def verify_and_add_img_and_filter_dims(img_dim, filter_dim):
+        # Verify set for img dim
+        if img_dim in rhs_dims or img_dim not in res_dims:
+            return None
+        output_img_dims.append(img_dim)
+        # Verify set for filter dim
+        if filter_dim not in rhs_dims or filter_dim in res_dims:
+            return None
+        filter_loop_dims.append(filter_dim)
+
+    for expr in lhs_map.results:
+        dim_strs = str(expr).split(" ")
+        if len(dim_strs) == 1:
+            dim = get_dim_pos(dim_strs[0])
+            if dim is None:
+                return None
+            if dim not in rhs_dims and dim in res_dims:
+                batch_dims.append(dim)
+            elif dim in rhs_dims and dim not in res_dims:
+                input_channel_dims.append(dim)
+            else:
+                return None
+            lhs_dims.append([dim])
+            continue
+        # Convolved dim with no stride
+        if len(dim_strs) == 3:
+            if dim_strs[1] != "+":
+                return None
+            img_dim = get_dim_pos(dim_strs[0])
+            filter_dim = get_dim_pos(dim_strs[2])
+            if img_dim is None or filter_dim is None:
+                return None
+            verify_and_add_img_and_filter_dims(img_dim, filter_dim)
+            strides.append(1)
+            dilations.append(1)
+            lhs_dims.append([img_dim, filter_dim])
+            continue
+        # Convolved dim with stride
+        if len(dim_strs) == 5:
+            if dim_strs[1] != "*" or dim_strs[3] != "+":
+                return None
+            img_dim = get_dim_pos(dim_strs[0])
+            filter_dim = get_dim_pos(dim_strs[4])
+            if img_dim is None or filter_dim is None:
+                return None
+            verify_and_add_img_and_filter_dims(img_dim, filter_dim)
+            if not dim_strs[2].isdigit():
+                return None
+            strides.append(int(dim_strs[2]))
+            dilations.append(1)
+            lhs_dims.append([img_dim, filter_dim])
+            continue
+    
+    # lhs_dims is a 2D array. Get the set of dims lhs dims.
+    lhs_dim_set = set()
+    for dims in lhs_dims:
+        for dim in dims:
+            lhs_dim_set.add(dim)
+    # Collect output channel dims
+    for d in range(rhs_map.n_dims):
+        if d not in lhs_dim_set and d in rhs_dims and d in res_dims:
+            output_channel_dims.append(d)
+    
+    # Verify that there are convolved dims
+    if len(filter_loop_dims) == 0:
+        return None
+
+    return (
+        ConvolutionDimensions(
+            batch=batch_dims,
+            outputImage=output_img_dims,
+            outputChannel=output_channel_dims,
+            filterLoop=filter_loop_dims,
+            inputChannel=input_channel_dims,
+            strides=strides,
+            dilations=dilations,
+        ),
+        lhs_dims,
+        [[d] for d in rhs_dims],
+        [[d] for d in res_dims],
+    )
 
 
 class ContractionOpInterfaceMatcher(GenericOpMatcher):
@@ -175,6 +277,41 @@ class ContractionOpInterfaceMatcher(GenericOpMatcher):
             k=k_dims,
             batch=batch_dims,
         )
+        self.lhs_dims = lhs_dims
+        self.rhs_dims = rhs_dims
+        self.res_dims = res_dims
+        return True
+
+
+class ConvolutionOpInterfaceMatcher(GenericOpMatcher):
+    def __init__(self) -> None:
+        super().__init__()
+        self.supported_named_ops = ["linalg.conv_2d_nhwc_hwcf"]
+        self.op_names.extend(self.supported_named_ops)
+        self.convolution_dimensions: Optional[ConvolutionDimensions] = None
+        self.lhs_dims: Optional[list[list[int]]] = None
+        self.rhs_dims: Optional[list[list[int]]] = None
+        self.res_dims: Optional[list[list[int]]] = None
+
+    def match_operands(self, operands: ir.OpOperandList) -> bool:
+        if len(operands) != 3:
+            return False
+        for operand in operands:
+            if not isinstance(operand.type, ir.ShapedType):
+                return False
+        return True
+
+    def match_indexing_maps(self, maps: list[ir.AffineMap]) -> bool:
+        if len(maps) != 3:
+            return False
+
+        conv_dim_info = get_convolution_dims(maps[0], maps[1], maps[2])
+        if conv_dim_info is None:
+            return False
+
+        conv_dims, lhs_dims, rhs_dims, res_dims = conv_dim_info
+        self.convolution_dimensions = conv_dims
+
         self.lhs_dims = lhs_dims
         self.rhs_dims = rhs_dims
         self.res_dims = res_dims
