5a2fab04b87ee36f5679085476c6204417dd60c9
Ean Garvey <ean.garvey@amd.com>
2025-03-06 14:22:17 -0800

    Restrict to compile/precompiled and update bucket versions.

--- shortfin/python/shortfin_apps/sd/components/builders.py
+++ shortfin/python/shortfin_apps/sd/components/builders.py
@@ -30,7 +30,7 @@ dtype_to_filetag = {
     sfnp.bfloat16: "bf16",
 }
 
-ARTIFACT_VERSION = "11182024"
+ARTIFACT_VERSION = "03062025"
 SDXL_BUCKET = (
     f"https://sharkpublic.blob.core.windows.net/sharkpublic/sdxl/{ARTIFACT_VERSION}/"
 )
