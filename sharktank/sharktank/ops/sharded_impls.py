59c4c7c2a6aa86df99a4bdd5c42998719a45cdb0
Boian Petkantchin <boian.petkantchin@amd.com>
2025-03-28 01:03:28 +0000

    Fix 2

--- sharktank/sharktank/ops/sharded_impls.py
+++ sharktank/sharktank/ops/sharded_impls.py
@@ -539,11 +539,21 @@ def expand_split(
     assert len(shape) == len(tensor.shape)
     shard_dim = tensor.shard_dim
     not_expanding_split_dim = (
-        shape[shard_dim] == -1 or shape[shard_dim] == tensor.shards[0].shape[shard_dim]
+        shape[shard_dim] == -1 or shape[shard_dim] == tensor.shape[shard_dim]
     )
     assert not_expanding_split_dim, "Expanding a split dimension is not supported"
 
-    shards = [expand(shard, shape) for shard in tensor.shards]
+    def set_element(l: List, idx: int, el: Any) -> List:
+        l[idx] = el
+        return l
+
+    shards = [
+        expand(
+            shard,
+            set_element(list(shape), tensor.shard_dim, shard.shape[tensor.shard_dim]),
+        )
+        for shard in tensor.shards
+    ]
     return SplitPrimitiveTensor(ts=shards, shard_dim=tensor.shard_dim)
 
 
