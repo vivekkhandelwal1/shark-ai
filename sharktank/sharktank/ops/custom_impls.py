8989e96491b7ab9bd5ed5cfc2386dc1bc6e1cd5f
Kyle Herndon <kyle.herndon@amd.com>
2025-05-01 22:18:55 +0000

    Fix typo

--- sharktank/sharktank/ops/custom_impls.py
+++ sharktank/sharktank/ops/custom_impls.py
@@ -87,7 +87,7 @@ def elementwise_binary(operator, x, y, *args, **kwargs):
     if isinstance(y, Number):
         return NotImplemented
 
-    new_qs = elementwise_tensor_tensor(operator_map[operator], x, inp_y)
+    new_qs = elementwise_tensor_tensor(x, inp_y, operator_map[operator])
     layout = TensorScaledLayout(shape=x.shape, qs=new_qs, d=scale)
     return PlanarQuantizedTensor(shape=x.shape, layout=layout)
 
